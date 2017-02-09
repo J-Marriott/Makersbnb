f99b38df9ee5d45d3f790b96aa0aa4b1588de9cd
Matthew Perry <thesedatedprince@gmail.com>
2017-02-09 17:24:08 +0000

    minor change to less than available date

--- app/app.rb
+++ app/app.rb
@@ -50,12 +50,12 @@ class Makersbnb < Sinatra::Base
   post '/spaces/filter' do
     @first_night, @last_night= params[:first_night], params[:last_night]
     guest_date_range = [*@first_night.to_s..@last_night.to_s]
-    @spaces = Space.all(:available_start_date.lt => @first_night, :available_end_date.gt => @last_night)
+    @spaces = Space.all(:available_start_date.lte => @first_night, :available_end_date.gte => @last_night)
     request_date_range = []
     @spaces.each { |space|
         requests = Request.all(space_id: space.id)
         requests.each { |request|
-        request_date_range << [*request.check_in_date.to_s..request.check_out_date.to_s] }
+        request_date_range << [*request.check_in_date.to_s..(request.check_out_date - 1).to_s] }
         @spaces.delete(space) if !(request_date_range.flatten & guest_date_range).empty?
     }
     erb :'spaces/index'
